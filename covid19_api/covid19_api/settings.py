be28b834de2b0f90d5f317a89a3db6846dff8c10
Harrison Ndegwa <harrisonndegwa65@gmail.com>
2020-04-13 14:45:26 +0300

    Minor changes

--- covid19_api/covid19_api/settings.py
+++ covid19_api/covid19_api/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,5 +131,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static")
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
